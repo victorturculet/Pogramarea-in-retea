d3bd5d0429f873a1d50bd2e51537471f707d4386
victorturculet <55275711+victorturculet@users.noreply.github.com>
2020-05-28 11:54:01 +0300

    Create client_voice.java

---
+++ client_voice.java
@@ -0,0 +1,12 @@
+package src;
+
+
+public class Client_voice {
+
+    public static boolean calling = false;
+    public static void main(String[] args) {
+        client_fr fr = new client_fr();
+        fr.setVisible(true);
+    }
+
+}
